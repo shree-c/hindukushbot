14dc92dc7e6c1ffe027d746cb7998f524d486979
shree <mail@shree.com>
2021-06-09 21:16:08 +0530

    pushing it aws

--- app.js
+++ app.js
@@ -6,6 +6,36 @@ const { getArticleBody, realfilterfun } = require('./index');
 const breakStr = require('./brkstrfun');
 const apikey = process.env.TELE_KEY;
 const bot = new Telegraf(apikey);
+const schedule = require('node-schedule');
+const rule = new schedule.RecurrenceRule();
+rule.hour = 0; 
+rule.minute = 10;
+rule.second = 10;
+const job = schedule.scheduleJob(rule, async () => {
+    try {
+        const chatids = await db.collection('ids').find({})
+        await chatids.forEach(async (obj) => {
+            console.log(obj)
+            await bot.telegram.sendMessage(obj.id, 'hello world')
+            let value = await getCachedArticleBody('lead')
+            for (const bobj of value) {
+                for (const sobj of bobj) {
+                    await bot.telegram.sendMessage(obj.id, sobj)
+                }
+            }
+            value = await getCachedArticleBody('editorial')
+            for (const bobj of value) {
+                for (const sobj of bobj) {
+                    await bot.telegram.sendMessage(obj.id, sobj)
+                }
+            }
+        })
+
+    } catch(err) {
+        console.log(err)
+    }
+
+})
 
 bot.command('gettodayslead', (ctx) => {
     getCachedArticleBody('lead').then(async (value) => {
@@ -38,12 +68,23 @@ bot.command('start', async (ctx) => {
     I can give you todays lead and editorial articles from the hindu.
     gettodayseditorials and gettodayslead are respective commands.
     try typing /`);
-        await db.collection('ids').insertOne({ id: ctx.chat.id });
+        //checking if id already exists
+        const item = await db.collection('ids').findOne({ id: ctx.chat.id })
+        if (!item)
+            await db.collection('ids').insertOne({ id: ctx.chat.id });
+        else {
+            console.log('id exist')
+            ctx.reply('you are already regestered')
+        }
+
     } catch (err) {
         console.log(err)
     }
 })
-
+bot.command('stop', async (ctx) => {
+    await db.collection('ids').deleteOne({ id: ctx.chat.id })
+    ctx.reply('ok removed!')
+})
 function getCachedArticleBody(section) {
     return new Promise((resolve, reject) => {
         // console.log(getCachedArticleBody.updatedTime[section])
